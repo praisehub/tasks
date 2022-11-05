872604b985997605411671087014927982232cb4
praisehub <praiseemmanuel9ic@gmail.com>
2022-11-05 11:49:02 +0100

    first commit

---
+++ lesson2.py
@@ -0,0 +1,56 @@
+# tuple= (1,2,3,4,5,6)
+# print(tuple[0])
+# tuple2 = ('win', 'sing', 'rain')
+# print(tuple2[1])
+# tuple = tuple + tuple2
+# print(tuple)
+# tuple3 = ('marry', 'anna', 'marry', 'anne', 'every', 'week')
+# print(tuple3)
+
+# dictionary = ('allow', 'me', 'the', 'honor', 'of', 'learning')
+# print(dictionary)
+# dict = {'marry':'anna', 'marry':'anne', 'every':'week'}
+# print(dict['marry'])
+#  #FUNCTIONS
+# def numbers_sum (a,b):
+#     c = a+b
+#     return c
+
+# myfamily = {
+#   "child1" : [1,2,3],
+#   "child2" : {
+#     "name" : "Tobias",
+#     "year" : 2007
+#   },
+#   "child3" : {
+#     "name" : "Linus",
+#     "year" : 2011
+#   }
+# }
+
+# print(myfamily['child2'])
+# print(myfamily["child3"]['year'])
+
+### Indexing a list
+# fruit_list = ["apple", "banana", "cherry"]
+# print(f"Second element: {fruit_list[1]}")
+# print(f"first element: {fruit_list[0]}")
+# print(f"last element: {fruit_list[-1]}")
+# print(f"1 and 3: {fruit_list[0:2]}")
+
+#increase by duplicating the elements of lists
+#print([1,2,3]*3)
+#increase by extending by an element to end of list
+# fruit_list.append("orange")
+# print(fruit_list)
+# # add list to list
+# tropical = ["mango", "pineapple", "papaya",'mango']
+# fruit_list.extend(tropical)
+# print(fruit_list)
+
+# for x in range(5):
+#   for y in range(3):
+#     print (f"({x},{y})")
+
+for x in 'memorandum':
+      print(x)
---
+++ lessson1.py
@@ -0,0 +1,14 @@
+#conditional statements
+# if and else
+num = 10
+if num > 11:
+    print('num is less than 11')
+else:
+    print('num is not less than 11')
+
+#while loops
+for i in range(10): #[0,1,2, ...9]
+    if i % 2 == 0:# if i is an even number
+       print(i) #reserved words
+import pandas as pd
+df = pd.read_excel()
